5284e1860eed4be72ca70d8ae66a3fdd0388071b
Adam Barnhard <admbarn@gmail.com>
2014-11-04 11:37:30 -0600

    corrected filepaths

--- main.js
+++ main.js
@@ -13,10 +13,10 @@ var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv'),
 
 function preload(){
   game.stage.backgroundColor = '#71c5cf';
-  game.load.spritesheet('block', '/assets/sprite.png', 24, 24);
-  game.load.spritesheet('pipe', '/assets/pipes.png', 52, 320);
-  game.load.spritesheet('boom', '/assets/explosion.png', 40, 40);
-  game.load.audio('bgsound', ['/assets/audio/oedipus_wizball_highscore.mp3', '/assets/audio/oedipus_wizball_highscore.ogg']);
+  game.load.spritesheet('block', 'assets/sprite.png', 24, 24);
+  game.load.spritesheet('pipe', 'assets/pipes.png', 52, 320);
+  game.load.spritesheet('boom', 'assets/explosion.png', 40, 40);
+  game.load.audio('bgsound', ['assets/audio/oedipus_wizball_highscore.mp3', 'assets/audio/oedipus_wizball_highscore.ogg']);
 }
 
 function create(){
